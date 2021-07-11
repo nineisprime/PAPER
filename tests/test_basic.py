38c6405d38318c9439bad25f41053188e410da80
Min Xu <minx@Mins-MacBook-Pro-2.local>
2021-07-11 18:06:07 -0400

    restructuring

---
+++ tests/test_basic.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 15:54:58 2021
+
+@author: minx
+"""
+
+
+from PAPER.tree_tools import *
+import numpy as np
+import pickle
+from PAPER.gibbsSampling import *
+from PAPER.grafting import *
+from PAPER.estimateAlpha import *
+
+
+def test_gibbs_single_root():
+
+    n = 50
+    K = 1
+    m = 53
+    
+    alpha = 0
+    beta = 1
+
+    graf = createNoisyGraph(n, m, alpha=alpha, beta=beta, K=K)[0]
+        
+    res = gibbsToConv(graf, Burn=20, M=40, DP=False, method="full",
+                                   K=1, alpha=alpha, beta=beta, tol=0.05)
+        
+    assert abs(sum(res[0]) - 1) < 1e-3
+            
+            
+    res2 = gibbsToConv(graf, Burn=20, M=40, DP=False, method="collapsed",
+                                   K=1, alpha=alpha, beta=beta, tol=0.05)
+    
+    assert sum( np.abs( res[0] - res2[0] ))/2 < 0.2
+    
+    
+    
+def test_gibbs_single_root_with_estimation():
+    
+    n = 300
+    K = 1
+    m = 500
+    
+    alpha = 1
+    beta = 1
+
+    graf = createNoisyGraph(n, m, alpha=alpha, beta=beta, K=K)[0]
+    alphahat = estimateAlphaEM(graf, display=False)
+    assert abs(alphahat - alpha) < 3
+
+
+    n = 50
+    m = 52
+    graf = createNoisyGraph(n, m, alpha=alpha, beta=beta, K=K)[0]
+        
+    res = gibbsToConv(graf, Burn=20, M=40, DP=False, method="full",
+                                   K=1, alpha=0, beta=0, tol=0.02)
+        
+    assert abs(sum(res[0]) - 1) < 1e-3
+            
+            
+    res2 = gibbsToConv(graf, Burn=20, M=40, DP=False, method="collapsed",
+                                   K=1, alpha=0, beta=0, tol=0.02)
+    
+    assert sum( np.abs( res[0] - res2[0] ))/2 < 0.2    
+    
+    
+    
+def test_fixed_K_roots():
+    
+    n = 50
+    K = 2
+    m = 70
+    
+    alpha = 1
+    beta = 1
+
+    graf = createNoisyGraph(n, m, alpha=alpha, beta=beta, K=K)[0]
+        
+    res = gibbsToConv(graf, Burn=20, M=40, DP=False, method="full",
+                                   K=K, alpha=alpha, beta=beta, tol=0.05,
+                                   birth_thresh=1)
+        
+    assert abs(sum(res[0]) - 1) < 1e-3
+            
+            
+    res2 = gibbsToConv(graf, Burn=20, M=40, DP=False, method="collapsed",
+                                   K=K, alpha=alpha, beta=beta, tol=0.05,
+                                   birth_thresh=1)
+    
+    assert sum( np.abs( res[0] - res2[0] ))/2 < 0.3
+    
+    
+def test_random_K_roots():
+    
+    n = 50
+    K = 2
+    m = 70
+    
+    alpha = 1
+    beta = 1
+
+    graf = createNoisyGraph(n, m, alpha=alpha, beta=beta, K=K)[0]
+        
+    res = gibbsToConv(graf, Burn=20, M=40, DP=True, method="full",
+                                   alpha=alpha, beta=beta, tol=0.05,
+                                   birth_thresh=1)
+        
+    assert abs(sum(res[0]) - 1) < 1e-3
+            
+            
+    res2 = gibbsToConv(graf, Burn=20, M=40, DP=True, method="collapsed",
+                                   alpha=alpha, beta=beta, tol=0.05,
+                                   birth_thresh=1)
+    
+    assert sum( np.abs( res[0] - res2[0] ))/2 < 0.3    
